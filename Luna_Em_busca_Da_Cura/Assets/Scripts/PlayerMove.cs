c10f1d89c19c2bbdc20b7b5d54e92488138b97e3
Lucas Santos <santos94us@hotmail.com>
2023-02-04 01:10:17 -0300

    add jump v1 and fix slipping when move

--- Luna_Em_busca_Da_Cura/Assets/Scripts/PlayerMove.cs
+++ Luna_Em_busca_Da_Cura/Assets/Scripts/PlayerMove.cs
@@ -2,22 +2,56 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float speed = 20f;
+    public Rigidbody2D Rb;
+    public float Speed = 20f;
+    public float JumpForce = 2000f;
+    public bool Grounded;
+    public LayerMask GroundLayers; 
 
     public void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        Rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        VerifyGrounded();
     }
 
     private void FixedUpdate()
     {
-        var getAxys = Input.GetAxis("Horizontal");
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        var getAxys = Input.GetAxisRaw("Horizontal");
         if (getAxys > 0)
-            rb.velocity = new Vector2(speed, 0);
+            Rb.velocity = new Vector2(Speed, 0);
         else if (getAxys < 0)
-            rb.velocity = new Vector2(-speed, 0);
+            Rb.velocity = new Vector2(-Speed, 0);
         else
-            rb.velocity = Vector2.zero;
+            Rb.velocity = Vector2.zero;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButton("Jump") && Grounded)
+        {
+            Rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void VerifyGrounded()
+    {
+        Grounded = Physics2D.OverlapArea(new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f),
+           new Vector2(transform.position.x + 0.5f, transform.position.y - 51f), GroundLayers).IsTouchingLayers();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y - 0.505f), new Vector2(1f, 0.01f));
     }
 }
