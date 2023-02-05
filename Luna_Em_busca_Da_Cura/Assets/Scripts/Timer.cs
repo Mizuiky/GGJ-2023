d477b4dc106cf7643157620e78e75b5a2c6cf7ba
Lucas Santos <santos94us@hotmail.com>
2023-02-05 14:35:49 -0300

    add timer script

---
+++ Luna_Em_busca_Da_Cura/Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timer = 0f;
+    public bool IsPlay = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (IsPlay)
+            timer += Time.deltaTime;
+    }
+
+    public void Play()
+    {
+        IsPlay = true;
+    }
+
+    public void Pause()
+    {
+        IsPlay = false;
+    }
+}
